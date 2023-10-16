ea1bf0da336f3edb6e0075956fbad04829a6e908
A-Marvulle <alfredo.marvulle@gmail.com>
2023-10-16 18:55:03 -0300

    contador ver 2

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contador',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Conter App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _welcome = 'Aperte o Botão para Iniciar';
+  int _sum = 0;
+  int _mult = 1;
+  String _resp = '';
+  
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      _sum = _sum + _counter;
+      _mult = _mult * _counter;
+      _welcome = '';
+      _resp = 'Apertou o Botão: $_counter''\n\nSoma: $_sum''\n\nMult: $_mult';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _welcome,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(
+              _resp,
+              style: Theme.of(context).textTheme.headlineMedium,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Add 1',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
